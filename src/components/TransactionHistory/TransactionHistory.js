1f7a81858232a54167b34904fb0f1205ac215e03
Julia Lavrenko <lavrenkojulia@gmail.com>
2021-03-04 18:50:11 +0100

    TransactionHistory added

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './TransactionHistory.module.css';
 
-const TransactionHistory = (items) => {
+const TransactionHistory = ({ items }) => {
   return (
-    <>
-      <h2>Transaction History</h2>
-    </>
+    <table className={s.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td className={s.type}>{type}</td>
+            <td className={s.amount}>{amount}</td>
+            <td className={s.currency}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
 export default TransactionHistory;
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
